3fd8eb4ce9666f4a93c544a274297ff2579c6020
swg <samrosenfield22@gmail.com>
2020-12-06 21:07:31 -0600

    print in color *should* work on windows and nix*

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@
 SOURCES = main.c interpreter.c lexer/lexer.c lexer/regex.c symbol.c parser/grammar.c parser/recdesc.c parser/ptree.c parser/semantic.c parser/typesys.c intermediate.c simulator.c ds/tree.c ds/vector.c lexer/nfa/nfa_build.c lexer/nfa/nfa_run.c lexer/nfa/stack/stack.c utils/misc.c utils/printcolor.c
 FLAGS = -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter -ggdb
 
-ifdef OS
-	EXEC = swgcc.exe
-else
+ifdef __linux__
 	EXEC = swgcc
+else
+	EXEC = swgcc.exe
 endif
 
 hellomake:
--- utils/printcolor.h
+++ utils/printcolor.h
@@ -6,23 +6,7 @@
 extern FILE *outfile;
 extern const char *outname;
 
-//background can be set with \033[fg;bgm
-//the set /p suppresses the newline
-#define RED_FONT        "echo|set /p= \033[31m"
-#define GREEN_FONT      "echo|set /p= \033[32m"
-#define YELLOW_FONT     "echo|set /p= \033[33m"
-#define BLUE_FONT       "echo|set /p= \033[34m"
-#define MAGENTA_FONT    "echo|set /p= \033[35m"
-#define CYAN_FONT       "echo|set /p= \033[36m"
-#define WHITE_FONT      "echo|set /p= \033[37m"
-#define RESET_FONT  	"echo|set /p= \033[0m"
-
-#define set_text_color(color)	system(color);
-
-/*#define printf(...)	({int cc = printf(__VA_ARGS__); if(outfile) fprintf(outfile, __VA_ARGS__); cc;})
-#define puts(s)		({int cc = puts(s); if(outfile) {fputs(s, outfile); fputc('\n', outfile);} cc;})
-#define putchar(c)	({int cc = putchar(c); if(outfile) fputc(c, outfile); cc;})*/
-
+//redefine print functions for logging
 #define printf(...)	({int cc = printf(__VA_ARGS__); print_to_log_file(__VA_ARGS__); cc;})
 #define puts(s)		({int cc = puts(s); print_to_log_file("%s\n", s); cc;})
 #define putchar(c)	({int cc = putchar(c); print_to_log_file("%c", c); cc;})
@@ -35,6 +19,26 @@ extern const char *outname;
 		assert(outfile);				\
 	}									\
 })
+
+#ifdef __linux__
+	#define PRINTTERM	"echo "
+#else		//windows
+	#define PRINTTERM	"echo|set /p= "	//set /p suppresses the newline
+#endif
+
+//background can be set with \033[fg;bgm
+//the set /p suppresses the newline
+#define RED_FONT        PRINTTERM "\033[31m"
+#define GREEN_FONT      PRINTTERM "\033[32m"
+#define YELLOW_FONT     PRINTTERM "\033[33m"
+#define BLUE_FONT       PRINTTERM "\033[34m"
+#define MAGENTA_FONT    PRINTTERM "\033[35m"
+#define CYAN_FONT       PRINTTERM "\033[36m"
+#define WHITE_FONT      PRINTTERM "\033[37m"
+#define RESET_FONT  	PRINTTERM "\033[0m"
+
+#define set_text_color(color)	system(color);
+
 
 #define printfcol(color, ...)			\
 	({								\
